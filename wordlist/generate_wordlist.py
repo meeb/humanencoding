85c8467c009cd217cfbc6dd2a26fb3cdabfa6cf2
meeb <meeb@meeb.org>
2017-01-03 13:07:34 +1100

    base wordlist v1 with tools

---
+++ wordlist/generate_wordlist.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+
+'''
+    This script parses the standard American English dictionary and attempts to
+    naively extract the easiest to pronounce words which do not contain
+    punctuation. There is a simple blacklist of reserved, abusive and commonly
+    used brand words applied to the dictionary. The resulting wordlist is
+    written to stdout.
+
+    Example:
+        $ python3 generate_wordlist.py > wordlist.txt
+'''
+
+
+import sys
+import string
+import re
+
+
+AMERICAN_WORDLIST = 'american-english-words.txt'
+BLACKLIST = 'blacklist.txt'
+MIN_WORD_LEN = 2
+MAX_WORD_LEN = 12
+MAX_FRAGMENT_REPITITIONS = 3
+PENALTY_PREFIXES = ('q', 'x', 'z', 'rh', 'ae', 'wr')
+PENALTY_AMOUNT = 1.2
+VOWELS = ('a', 'e', 'i', 'o', 'u', 'y')
+NUM_WORDS = 65536
+
+
+def is_digit(char):
+    return char in string.digits
+
+
+def is_lowercase_word(word):
+    has_vowel = False
+    for char in word:
+        if char not in string.ascii_lowercase:
+            return False
+        if char in VOWELS:
+            has_vowel = True
+    return has_vowel
+
+
+def read_file(filename):
+    r = []
+    with open(filename, 'rt') as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            r.append(line)
+    return r
+
+
+def number_of_syllables(word):
+    count = 0
+    if len(word) == 0:
+        return 0
+    if word[0] in VOWELS:
+        count += 1
+    for i in range(1, len(word)):
+        if word[i] in VOWELS and word[i - 1] not in VOWELS:
+            count += 1
+    if word.endswith('e'):
+        count -= 1
+    if word.endswith('le'):
+        count += 1
+    if count == 0:
+        count += 1
+    return count
+
+
+_repititions = re.compile(r'(.+?)\1+')
+
+
+def number_of_repetitions(word):
+    for match in _repititions.finditer(word):
+        yield (match.group(1), len(match.group(0))/len(match.group(1)))
+
+
+_blacklist = set(read_file(BLACKLIST))
+
+
+def clean_word(word):
+    word = str(word).lower().strip()
+    if not word:
+        return
+    elif not is_lowercase_word(word):
+        return
+    elif len(word) < MIN_WORD_LEN or len(word) > MAX_WORD_LEN:
+        return
+    elif word in _blacklist:
+        return
+    for (fragment, repeated) in number_of_repetitions(word):
+        if repeated > MAX_FRAGMENT_REPITITIONS:
+            return
+    return word
+
+
+def word_score(word):
+    syllables = number_of_syllables(word)
+    if not syllables:
+        return
+    score = syllables * len(word)
+    for prefix in PENALTY_PREFIXES:
+        if word[:len(prefix)] == prefix:
+            score *= PENALTY_AMOUNT
+            break
+    return score
+
+
+if __name__ == '__main__':
+    # process the words into a unique set
+    uniquewords = set()
+    for word in read_file(AMERICAN_WORDLIST):
+        word = clean_word(word)
+        if not word:
+            continue
+        uniquewords.add(word)
+    # give each word a score
+    scoredwords = []
+    for word in uniquewords:
+        score = word_score(word)
+        if not score:
+            continue
+        scoredwords.append((word, score))
+    # order the words by their score
+    orderedwords = sorted(scoredwords, key=lambda x: x[1])
+    # truncate at 65536
+    orderedwords = orderedwords[:NUM_WORDS]
+    # re-order alphabetically and strip the scores
+    words = sorted([word[0] for word in orderedwords])
+    # write the words to stdout
+    for word in words:
+        sys.stdout.write(word + '\n')
---
+++ wordlist/wordlist_to_code.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+'''
+    This script parses a wordlist in a file (one word per line split by
+    newlines) into a code structure. By default this is a Python tuple. The
+    generated data structure is written to stdout.
+
+    Example:
+        $ python3 wordlist_to_code.py -w wordlist_v1 -l python > words.py
+'''
+
+
+import os
+import sys
+import argparse
+from textwrap import wrap
+
+
+LANGUAGES = {
+    # generates a Python tuple
+    'python': {
+        'name': 'words',
+        'equals': '=',
+        'start_structure': '(',
+        'end_structure': ')',
+        'wrap_item': '\'',
+        'separate_item': ',',
+        'line_length': 79,
+    },
+    # generates an importable lua table
+    'lua': {
+        'name': 'return',
+        'equals': '',
+        'start_structure': '{',
+        'end_structure': '}',
+        'wrap_item': '\'',
+        'separate_item': ',',
+        'line_length': 80,
+    }
+}
+
+if __name__ == '__main__':
+    language_choices = [k.lower() for k in LANGUAGES.keys()]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--language', type=str, choices=language_choices,
+                        default='python',
+                        help='Select the output language')
+    parser.add_argument('-w', '--wordlist', type=str, default='wordlist.txt',
+                        help='Input wordlist file')
+    args = parser.parse_args()
+    language = args.language
+    wordlist_file = args.wordlist
+    params = LANGUAGES.get(language, None)
+    if not params:
+        raise Exception('Invalid language: {}'.format(language))
+    if not os.path.isfile(wordlist_file):
+        raise Exception('Not a file: {}'.format(wordlist_file))
+    words = []
+    with open(wordlist_file, 'rt') as wordlist:
+        for word in wordlist:
+            word = word.strip()
+            if word and not word.startswith('#'):
+                words.append(params['wrap_item'] + word + params['wrap_item'])
+    data_start = params['name'] + ' ' + params['equals']
+    if params['equals']:
+        data_start += ' '
+    data_start += params['start_structure']
+    spaces = ' ' * len(data_start)
+    data_line_len = params['line_length'] - len(data_start)
+    sep = params['separate_item'] + ' '
+    data_body = sep.join([w for w in words])
+    data_wrapped = wrap(data_body, width=data_line_len)
+    first_line = data_wrapped.pop(0)
+    last_line = data_wrapped.pop()
+    sys.stdout.write(data_start)
+    sys.stdout.write(first_line + '\n')
+    for line in data_wrapped:
+        sys.stdout.write(spaces + line + '\n')
+    sys.stdout.write(spaces + last_line)
+    if len(last_line) + len(spaces) >= params['line_length']:
+        sys.stdout.write('\n' + spaces + params['end_structure'] + '\n')
+    else:
+        sys.stdout.write(params['end_structure'] + '\n')
